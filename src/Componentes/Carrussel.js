f96b6f495e7f682e87748390ca5370e2535fa4b7
vinitenconi <tenconivini@gmail.com>
2022-04-17 22:29:51 -0300

    PruebaNetlify

--- src/Componentes/Carrussel.js
+++ src/Componentes/Carrussel.js
@@ -28,49 +28,50 @@ const Carrussel = () => {
         }else{
         return (
         <div id='containerPrincipal'>
-        <h2 style={{"text-align": "center", "font-size" : "3rem"}}>Tienda oficial del MaFaVi</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et turpis id lectus facilisis laoreet. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ultricies feugiat blandit. Fusce sed nunc leo. Sed vel nibh pulvinar, egestas dolor at, egestas felis. Aliquam molestie, lacus nec tempor euismod, mauris tellus suscipit purus, quis gravida libero lectus eget nisl. Vestibulum at sem aliquet, tincidunt orci scelerisque, blandit elit. Quisque sed velit quis eros aliquam sodales vehicula eget magna.</p>
-        <h4 style={{"text-align": "center"}}>Conocenos a traves de nuestros productos, por eso, te mostramos los más requeridos de cada categoría!</h4>
-        <Container className='carrussel'>
-            <Carousel fade={true} interval= {4000} >
-                <Carousel.Item>
-                    <img
-                    className="d-block w-100 imgC"
-                    src="/imagenesDelProyecto/CamisetaS.jpg"
-                    alt="First slide"
-                    />
-                    <Carousel.Caption className='textCar'>
-                    <h3>Camiseta titular</h3>
-                    <p>Azul y negro como nuestra sangre. Nuestro manto sagrado.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                    className="d-block w-100 imgC"
-                    src="/imagenesDelProyecto/Vasavi.jpg"
-                    alt="Second slide"
-                    />
-                    <Carousel.Caption className='textCar'>
-                    <h3>Vasavi</h3>
-                    <p>De acero inoxidable, nuestro vaso te acompaña de día y de noche ;).</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item className='nft'>
-                    <>
-                    <div className='nft'>
-                    <img
-                    className="d-block imgC"
-                    src="/imagenesDelProyecto/MafaSegundoEscudo.jpg"
-                    alt="Third slide"
-                    style={{"width" : "38rem !important"}}
-                    />
-                    <Carousel.Caption className='textCar'>
-                    <h3>NFT's</h3>
-                    <p>Proximamente en nuestra tienda, te mostramos lo que va  venir.</p>
-                    </Carousel.Caption></div></>
-                </Carousel.Item>
-            </Carousel>
-    </Container>
+            <h2 style={{"text-align": "center", "font-size" : "3rem"}}>Tienda oficial del MaFaVi</h2>
+            <p>En una noche oscura y fría, allá por Septiembre de 2015 en la Ciudad de Bariloche, nuestro viejo y querido amigo Hiroshi nos colocó un mote que, aunque no lo supieramos, nos iba a quedar de por vida: "Manga de fanáticos del vino". Muy acertado estuvo él al momento de recitar esas palabras, pero lo que no sabía era que el fanatismo iba más alla del jugo de uva.</p>
+            <p>Luego de seis años del momento de nuestra creación decidimos crear la Tienda Oficial del Mafavi. Porque la pasión se contagia, queremos que todos tengan la posibilidad de tener los productos de esta familia. El Mafa es nacional y popular, no tiene hinchas...tiene fanáticos.</p>
+            <h4 style={{"text-align": "center"}}>Conocenos a traves de nuestros productos, por eso, te mostramos los más requeridos de cada categoría!</h4>
+            <Container className='carrussel'>
+                <Carousel fade={true} interval= {4000} >
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100 imgC"
+                        src="/imagenesDelProyecto/CamisetaS.jpg"
+                        alt="First slide"
+                        />
+                        <Carousel.Caption className='textCar'>
+                        <h3>Camiseta titular</h3>
+                        <p>Azul y negro como nuestra sangre. Nuestro manto sagrado.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100 imgC"
+                        src="/imagenesDelProyecto/Vasavi.jpg"
+                        alt="Second slide"
+                        />
+                        <Carousel.Caption className='textCar'>
+                        <h3>Vasavi</h3>
+                        <p>De acero inoxidable, nuestro vaso te acompaña de día y de noche ;).</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item className='nft'>
+                        <>
+                        <div className='nft'>
+                        <img
+                        className="d-block imgC"
+                        src="/imagenesDelProyecto/MafaSegundoEscudo.jpg"
+                        alt="Third slide"
+                        style={{"width" : "38rem !important"}}
+                        />
+                        <Carousel.Caption className='textCar'>
+                        <h3>NFT's</h3>
+                        <p>Proximamente en nuestra tienda, te mostramos lo que va  venir.</p>
+                        </Carousel.Caption></div></>
+                    </Carousel.Item>
+                </Carousel>
+            </Container>
     </div>)
 }}
 export default Carrussel
